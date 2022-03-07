a565b6b33526a03393260e9c61cc1ff55d220f47
lisasiva <lisashantisiva@gmail.com>
2022-03-07 11:12:48 -0600

    new comp

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import { ref } from "vue"
 import Storage from "@src/services/storage";
 import { fetchIpfsMeta } from "@src/services/helpers";
 import { ethers } from 'ethers'
-import contractABInsta from '../artifacts/contracts/InstagramPosting.sol/InstagramPosting.json'
 import contractABIrebel from '../artifacts/contracts/SocialHelper.sol/SocialHelper.json'
 
 const db = new Storage("app");
@@ -61,110 +60,6 @@ export const useStore = defineStore({
       db.data.results = [ ...this.results ];
       db.write();
     }
-  }
-});
-
-export const useInstaStore = defineStore('insta', () => {
-  const account = ref(null)
-  const indexCount = ref(0)
-  const postedItems = ref([])
-
-  async function postContent(imgHash, textHash) {
-    try {
-      const { ethereum } = window
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const instaContract = new ethers.Contract(contractAddressInsta, contractABInsta.abi, signer)
-        const posted = (await instaContract.sendHash(imgHash, textHash))
-        console.log('Mining...', posted.hash)
-        await posted.wait()
-        console.log('Mined -- ', posted.hash)
-      }
-    }
-    catch (e) {
-      console.log('e', e)
-    }
-  }
-
-  async function getContent(postIndex) {
-    try {
-      const { ethereum } = window
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const instaContract = new ethers.Contract(contractAddressInsta, contractABInsta.abi, signer)
-        const post = (await instaContract.getHash(postIndex));
-        postedItems.value.push(post);
-        console.log("got post", post.img)
-      }
-    }
-    catch (e) {
-      console.log('e', e)
-    }
-  }
-
-async function getAllPosts() {
-  try {
-    const { ethereum } = window
-    if (ethereum) {
-      const provider = new ethers.providers.Web3Provider(ethereum)
-      const signer = provider.getSigner()
-      const instaContract = new ethers.Contract(contractAddressInsta, contractABInsta.abi, signer)
-      console.log("got here")
-      for (let i = 0; i < indexCount.value; i++) {
-        await getContent(i);
-      }
-    }
-  }
-  catch (e) {
-    console.log('e', e)
-  }
-}
-
-  async function getNftIndex() {
-    try {
-      const { ethereum } = window
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const instaContract = new ethers.Contract(contractAddressInsta, contractABInsta.abi, signer)
-        indexCount.value = (await instaContract.getCounter()).toNumber()
-        console.log("got index count", indexCount.value)
-      }
-    }
-    catch (e) {
-      console.log('e', e)
-    }
-  }
-
-  async function connectWallet() {
-    try {
-      const { ethereum } = window
-      if (!ethereum) {
-        alert('Must connect to MetaMask!')
-        return
-      }
-      const myAccounts = await ethereum.request({ method: 'eth_requestAccounts' })
-
-      console.log('Connected: ', myAccounts[0])
-      account.value = myAccounts[0]
-      await getNftIndex();
-      await getAllPosts();
-    }
-    catch (error) {
-      console.log(error)
-    }
-  }
-
-  return {
-    connectWallet,
-    account,
-    postContent,
-    getContent,
-    getNftIndex,
-    indexCount,
-    postedItems
   }
 });
 
