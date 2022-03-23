2a47652d360b0f0b50712ea79d96a6ab6d4d084f
lisasiva <lisashantisiva@gmail.com>
2022-03-23 14:50:52 -0500

    more cleanup of deploy script w/ token seeding

--- scripts/deploy-rebel.js
+++ scripts/deploy-rebel.js
@@ -19,6 +19,13 @@ async function main() {
   const rebel = await RebelDapp.deploy(rebelToken.address, rebelCrowdsale.address);
   await rebel.deployed();
   console.log("Rebel DAPP deployed to:", rebel.address);
+
+  const AMT_TO_TRANSFER = "500000000.0"; // half of entire 1B token supply
+  await rebelToken.connect(deployer).transfer(rebel.address, ethers.utils.parseEther(AMT_TO_TRANSFER));
+  console.log("REBEL tokens seeded to main DAPP: √")
+
+  await rebelToken.connect(deployer).transfer(rebelCrowdsale.address, ethers.utils.parseEther(AMT_TO_TRANSFER));
+  console.log("REBEL tokens seeded to Crowdsale: √")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
