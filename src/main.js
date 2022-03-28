a42df9c5e5697add15d419eb2dcced6f85e63471
lisasiva <lisashantisiva@gmail.com>
2022-03-28 16:56:29 -0500

    fix package-lock file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { isRunningOnPWA } from "@src/services/helpers";
-import { createHead } from "@vueuse/head";
 
 import App from "@src/App.vue";
 import router from "@src/router";
@@ -14,7 +13,7 @@ import "notyf/notyf.min.css";
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(pinia).use(router).use(WaveSurferVue).use(createHead());
+app.use(pinia).use(router).use(WaveSurferVue);
 
 if (isRunningOnPWA()) window.resizeTo(985, 677);
 
